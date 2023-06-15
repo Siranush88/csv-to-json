445a802467b56d4d6d84b95137cd522e405494f7
Siranush <siranush.zakaryan.job@gmail.com>
2023-06-15 23:49:32 +0400

    csv files are parsed into json

--- app.js
+++ app.js
@@ -1,65 +1,60 @@
-//import path from 'path';
-// import fs from 'fs';
-// import csv from 'csv-parser';
-// import { writeFile } from 'fs/promises';
-//import { readdir } from 'fs/promises';
-//import cluster from 'cluster';
-//import os from 'os';
 const cluster = require('cluster')
 const path = require('path')
 const os = require('os')
-//const readdir =  require('fs/promises')
+const { readdir } =  require('fs/promises')
 const fs = require('fs')
 const csv = require('csv-parser')
 
-if (cluster.isPrimary) {
+async function csvToJson(directoryPath) {
 
-    const directoryPath = process.argv[2];
-    if (process.argv.length < 3) {
-        console.error('Not given the path to the folder');
-        process.exit(1);
-    }
+  if (process.argv.length < 3) {
+    console.error('Not given the path to the folder');
+    process.exit(1);
+  }
 
-    const csvFilesArray = fs.readdirSync(directoryPath);
-    const numCpus = os.cpus().length;
+  if (cluster.isPrimary) {
+    const csvFilesArray = await readdir(directoryPath);
 
-    let worker;
+    const numCPUs = os.cpus().length;
+    const workerNumber = Math.min(numCPUs, csvFilesArray.length);
+    let num = 0;
 
-    
     for (let i = 0; i < csvFilesArray.length; i++) {
-        worker = cluster.fork();
-        
+      const csvFilePath = path.join(directoryPath, csvFilesArray[i]);
+
+      if (num >= workerNumber) {
+        num = 0;
+      }
+
+      const worker = cluster.fork();
+      worker.send(csvFilePath);
+      num++;
     }
-
-    worker.on('online', (msg) => {
-        for (let i = 0; i < csvFilesArray.length; i++) {
-           let csvFilePath = path.join(directoryPath, csvFilesArray[i]);
-            worker.send(csvFilePath);
-        }
-    })
-} else {
-
+  } else {
     process.on('message', (message) => {
-        const filePath = message;
+      const filePath = message;
 
-        const startTime = new Date();
-        let count = 0;
+      const startTime = new Date();
+      let count = 0;
 
-        const pathChunk = filePath.split('\\')
-        const fileName = pathChunk[1].replace(".csv", ".json");
+      const pathChunk = filePath.split('\\');
+      const fileName = pathChunk[1].replace('.csv', '.json');
 
-        const results = [];
-        fs.createReadStream(filePath)
-            .pipe(csv())
-            .on('data', (data) => {
-                count++;
-                results.push(data);
-            })
-            .on('end', () => {
-                const endTime = new Date();
-                const duration = endTime - startTime;
-                console.log(`Parsing ${fileName} took ${duration} milliseconds to read ${count} lines`);
-                fs.writeFile(`./converted/${fileName}`, JSON.stringify(results, undefined, 2), 'utf-8', (data) => { })
-            })
+      const results = [];
+      fs.createReadStream(filePath)
+        .pipe(csv())
+        .on('data', (data) => {
+          count++;
+          results.push(data);
+        })
+        .on('end', () => {
+          const endTime = new Date();
+          const duration = endTime - startTime;
+          console.log(`It took ${duration} milliseconds to read ${count} lines of the  ${fileName} file`);
+          fs.writeFile(`./converted/${fileName}`, JSON.stringify(results, undefined, 2), 'utf-8', (data) => {});
+        });
     });
-}
+  }
+  }
+  
+csvToJson(process.argv[2])
